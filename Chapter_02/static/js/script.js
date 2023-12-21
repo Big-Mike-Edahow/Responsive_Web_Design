7d92199286944e844c3b15dfb93b00267ee9d6e7
Mike Jackson <perigran_falcon@yahoo.com>
2023-12-21 13:22:26 -0700

    Example source code from chapter 2.

---
+++ Chapter_02/static/js/script.js
@@ -0,0 +1,12 @@
+// script.js
+
+const dialogEle = document.getElementById("dialogEle");
+const launchBtn = document.getElementById("launchDialog");
+const formResult = document.getElementById("formResult");
+
+launchBtn.addEventListener("click", () => dialogEle.showModal());
+
+dialogEle.addEventListener("close", () => {
+  formResult.textContent = dialogEle.returnValue;
+});
+
